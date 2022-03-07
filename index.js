c7066b0ca7cbc95c5aad0f75c55dcd936a94cbbb
Yonle <yonle@duck.com>
2022-03-07 20:55:20 +0700

    Fixbug

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.get("/vi*", (req, res) => {
 });
 
 // Proxy to yt3.ggpht.com, Where User avatar is being stored on that host.
-app.get("/ytc/*", (req, res) => {
+app.get("/yt3/*", (req, res) => {
   let stream = miniget(`https://yt3.ggpht.com/${req.url}`, {
     headers: {
       "user-agent": user_agent,
